1bec5277a3722a140a5b49fab9ae4fa0a298eb29
Meghal Sadhu <sadhumeghal@gmail.com>
2026-08-05 10:26:54 +0530

    Import BaseModel in auth.py to fix 502 bad gateway

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, status, Request
+from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from collections import defaultdict
 from time import time
